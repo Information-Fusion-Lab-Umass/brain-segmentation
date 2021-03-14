f6801083735332680ca83a00a1ca46fc9cb58f58
James Ko <jamesqko@gmail.com>
2021-03-14 19:40:13 -0400

    Spacify, switch to BIDS path

--- getPicsOfHippo.py
+++ getPicsOfHippo.py
@@ -7,21 +7,24 @@ FSL_DIR = os.path.join(SCRIPT_DIR, 'fsl')
 RFA = os.path.join(FSL_DIR, 'bin', 'run_first_all')
 OUTPUT_DIR = os.path.join(SCRIPT_DIR, 'output')
 
-paths = pd.read_csv(os.path.join(SCRIPT_DIR, 'mriimg_meta_v5.csv'))['caps_path']
+paths = pd.read_csv(os.path.join(SCRIPT_DIR, 'mriimg_meta_v5.csv'))['bids_path']
 
 limit = int(os.environ['BS_LIMIT'])
 if limit is not None:
     paths = paths[:limit]
 
 for path in paths:
-	parts = os.path.basename(path).replace('.nii.gz', '').split('_')
-	patient_id = parts[0].replace('sub-ADNI', '')
-	viscode = parts[1].replace('ses-', '').lower()
-	output_dir = os.path.join(OUTPUT_DIR, patient_id, viscode)
+    parts = os.path.basename(path).replace('.nii.gz', '').split('_')
+    patient_id = parts[0].replace('sub-ADNI', '')
+    viscode = parts[1].replace('ses-', '').lower()
+    output_dir = os.path.join(OUTPUT_DIR, patient_id, viscode)
 
-	os.makedirs(output_dir, exist_ok=True)
-	os.chdir(output_dir)
-	os.system(f'{RFA} -i {path} -s L_Hipp,R_Hipp')
+    os.makedirs(output_dir, exist_ok=True)
+    os.chdir(output_dir)
+    prefix = f'{patient_id}-{viscode}'
+    cmd = f'{RFA} -i {path} -o {prefix} -s L_Hipp,R_Hipp'
+    print(f"Executing '{cmd}' from dir {output_dir}")
+    os.system(cmd)
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
