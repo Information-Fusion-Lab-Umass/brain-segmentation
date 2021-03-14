79d0ba47acf45ec89a08010a505320f411f3fa98
James Ko <jamesqko@gmail.com>
2021-03-14 18:05:30 -0400

    Fix some bugs

--- getPicsOfHippo.py
+++ getPicsOfHippo.py
@@ -2,19 +2,24 @@ import os
 import pandas as pd
 from datetime import datetime
 
-SCRIPT_DIR = os.path.dirname(__file__)
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+RFA = os.path.expanduser(os.path.join('~', 'Downloads', 'fsl', 'bin', 'run_first_all'))
 
-df = pd.read_csv('mriimg_meta_v5.csv')['caps_path']
+paths = pd.read_csv(os.path.join(SCRIPT_DIR, 'mriimg_meta_v5.csv'))['caps_path']
+
+limit = int(os.environ['BS_LIMIT'])
+if limit is not None:
+    paths = paths[:limit]
 
 for path in paths:
 	parts = os.path.basename(path).replace('.nii.gz', '').split('_')
 	patient_id = parts[0].replace('sub-ADNI', '')
-	visitCode = parts[1].replace('ses-', '').lower()
-	outputPath = os.path.join('./', patient_id, viscode))
-	os.makedirs(outputPath, exit_ok=True)
-	os.chrpath(outputPath)
-	os.system(f'run_first_all -i {path} -s L_Hipp,R_Hipp')
-	os.chrpath('./../../')
+	viscode = parts[1].replace('ses-', '').lower()
+	outputPath = os.path.join(SCRIPT_DIR, 'output', patient_id, viscode)
+
+	os.makedirs(outputPath, exist_ok=True)
+	os.chdir(outputPath)
+	os.system(f'{RFA} -i {path} -s L_Hipp,R_Hipp')
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
