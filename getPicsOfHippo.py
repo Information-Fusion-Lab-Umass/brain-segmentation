ba7da75241b05ca6b8026e7fc113a9859c443e8d
James Ko <jamesqko@gmail.com>
2021-03-14 18:10:50 -0400

    Fixes

--- getPicsOfHippo.py
+++ getPicsOfHippo.py
@@ -3,7 +3,9 @@ import pandas as pd
 from datetime import datetime
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
-RFA = os.path.expanduser(os.path.join('~', 'Downloads', 'fsl', 'bin', 'run_first_all'))
+FSL_DIR = os.path.join(SCRIPT_DIR, 'fsl')
+RFA = os.path.join(FSL_DIR, 'bin', 'run_first_all')
+OUTPUT_DIR = os.path.join(SCRIPT_DIR, 'output')
 
 paths = pd.read_csv(os.path.join(SCRIPT_DIR, 'mriimg_meta_v5.csv'))['caps_path']
 
@@ -15,10 +17,10 @@ for path in paths:
 	parts = os.path.basename(path).replace('.nii.gz', '').split('_')
 	patient_id = parts[0].replace('sub-ADNI', '')
 	viscode = parts[1].replace('ses-', '').lower()
-	outputPath = os.path.join(SCRIPT_DIR, 'output', patient_id, viscode)
+	output_dir = os.path.join(OUTPUT_DIR, patient_id, viscode)
 
-	os.makedirs(outputPath, exist_ok=True)
-	os.chdir(outputPath)
+	os.makedirs(output_dir, exist_ok=True)
+	os.chdir(output_dir)
 	os.system(f'{RFA} -i {path} -s L_Hipp,R_Hipp')
 
 now = datetime.now()
